c5e0d672055e25a50e95a6ee58d59d097550e1bf
Debroop Ghosh <debroopghosh2012@gmail.com>
2024-02-28 00:36:16 +0530

    final code

--- currency converter/src/components/inputBox.jsx
+++ currency converter/src/components/inputBox.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-
+import React, {useId} from 'react'
 
 function InputBox({
     label,
@@ -11,20 +10,17 @@ function InputBox({
     amountDisable = false,
     currencyDisable = false,
     className = "",
-
-    
-    className = "",
 }) {
-   
+   const amountInputId = useId()
 
     return (
-        <div className={`bg-white p-3 rounded-lg text-sm flex ${className} `}>
+        <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
             <div className="w-1/2">
-                <label  className="text-black/40 mb-2 inline-block">
+                <label htmlFor={amountInputId}  className="text-black/40 mb-2 inline-block">
                     {label}
                 </label>
                 <input
-                    
+                    id={amountInputId}
                     className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount"
@@ -38,17 +34,15 @@ function InputBox({
                 <select
                     className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
                     value={selectCurrency}
-                    onChange={(e) => onCurrencyChange && onCurrencyChange(Number(e.target.value))}
+                    onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
                     disabled={currencyDisable}
                 >
                     
                         {currencyOptions.map((currency) => (
-                            <option key={currency} 
-                            value=
-                            {currency}>
+                            <option key={currency} value={currency}>
                             {currency}
-                        </option>
-                        )) }
+                            </option>
+                        ))}
                 
                 </select>
             </div>
